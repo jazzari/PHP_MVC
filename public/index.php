bed03ece993aac5fc1683f18a123c855e544dc33
jazzari <passionjazz@gmail.com>
2021-07-29 15:49:21 +0200

    Move index.php to public directory

--- public/index.php
+++ public/index.php
@@ -1,10 +1,10 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 use app\core\Application;
 
 $app = new Application();
 
-$app->router->get('/', function(){
+$app->router->get('/home', function(){
     return 'Hello World';
 });
 $app->router->get('/contact', function(){
