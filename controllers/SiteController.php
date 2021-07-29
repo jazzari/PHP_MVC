c16de112a22f2674001eb9f265a99d5ee53deced
jazzari <passionjazz@gmail.com>
2021-07-29 20:09:08 +0200

    Add controller/actions to routes

---
+++ controllers/SiteController.php
@@ -0,0 +1,22 @@
+<?php
+namespace app\controllers;
+use app\core\Application;
+
+class SiteController{
+
+    public static function home(){
+        $params = [
+            'name' => "The PHPMVC"
+        ];
+        return Application::$app->router->renderView('home', $params);
+    }
+
+    public static function contact(){
+        return Application::$app->router->renderView('contact');
+    }
+
+    public static function handleContact(){
+        return 'Handling submitted data';
+    }
+}
+?>
--- public/index.php
+++ public/index.php
@@ -1,14 +1,13 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
+use app\controllers\SiteController;
 use app\core\Application;
 
 $app = new Application(dirname(__DIR__));
 
-$app->router->get('/home', 'home');
-$app->router->get('/contact', 'contact');
-$app->router->post('/contact', function(){
-    return "Handling submitted data";
-});
+$app->router->get('/home', [SiteController::class, 'home']);
+$app->router->get('/contact', [SiteController::class, 'contact']);
+$app->router->post('/contact', [SiteController::class, 'handleContact']);
 
 $app->run();
 ?>
