8a9d46e999c4feab725979d8b64aa9da9ce48c71
jazzari <passionjazz@gmail.com>
2021-07-30 09:26:51 +0200

    Create login and register pages and routes

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -8,13 +8,13 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>The PHPMVC</title>
   </head>
   <body>
 
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">The PHPMVC</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -25,6 +25,14 @@
         </li>
         <li class="nav-item">
           <a class="nav-link" href="/contact">Contact Us</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/login">Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/register">Register</a>
         </li>
       </ul>
     </div>
---
+++ views/register.php
@@ -0,0 +1,32 @@
+<h1>Create an Account</h1>
+
+<form action="" method="POST">
+  <div class="row">
+    <div class="col">
+    <div class="mb-3">
+    <label for="first" class="form-label">First Name</label>
+    <input type="text" class="form-control" id="first" name="first">
+  </div>
+    </div>
+    <div class="col">
+    <div class="mb-3">
+    <label for="last" class="form-label">Last Name</label>
+    <input type="text" class="form-control" id="last" name="last">
+  </div>
+    </div>
+  </div>
+  <div class="mb-3">
+    <label for="email" class="form-label">Email</label>
+    <input type="email" class="form-control" id="email" name="email">
+  </div>
+  <div class="mb-3">
+    <label for="password" class="form-label">Password</label>
+    <input type="password" class="form-control" id="password" name="password">
+  </div>
+  <div class="mb-3">
+    <label for="confirmation" class="form-label">Confirm Password</label>
+    <input type="password" class="form-control" id="confirmation" name="confirmation">
+  </div>
+  
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
