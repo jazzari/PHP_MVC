624ceb790cd09ba452a6f77c8f02d3009d967048
jazzari <passionjazz@gmail.com>
2021-08-02 12:17:46 +0200

    Create migration's classes part 2

--- core/Database.php
+++ core/Database.php
@@ -18,6 +18,7 @@ class Database{
         $this->createMigrationsTable();
         $appliedMigrations = $this->getAppliedMigrations();
 
+        $newMigrations = [];
         $files = scandir(Application::$ROOT_DIR . '/migrations');
         $toApplyMigrations = array_diff($files, $appliedMigrations);
         
@@ -31,6 +32,12 @@ class Database{
             echo "Applying migration $migration".PHP_EOL;
             $instance->up();
             echo "Applied migration $migration".PHP_EOL;
+            $newMigrations[] = $migration;
+        }
+        if (!empty($newMigrations)){
+            $this->saveMigrations($newMigrations);
+        } else {
+            echo "All migrations are applied";
         }
         
     }
@@ -47,6 +54,14 @@ class Database{
         $statement = $this->pdo->prepare("SELECT migration FROM migrations");
         $statement->execute();
         return $statement->fetchAll(\PDO::FETCH_COLUMN);
+    }
+
+    public function saveMigrations(array $migrations){
+        $str = implode(",", array_map(fn($m) => "('$m')", $migrations));
+
+        $statement = $this->pdo->prepare("INSERT INTO migrations (migration) VALUES
+        $str");
+        $statement->execute();
     }
 }
 ?>
