9d7dda62966c993ece360b703d860abd99f59804
jazzari <passionjazz@gmail.com>
2021-08-02 14:46:04 +0200

    Add table users to DB with migrations

--- migrations/m0001_initial.php
+++ migrations/m0001_initial.php
@@ -2,11 +2,22 @@
 
 class m0001_initial{
     public function up(){
-        echo "Applying migration".PHP_EOL;
+        $db = app\core\Application::$app->db;
+        $SQL = "CREATE TABLE users (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            email varchar(255) NOT NULL,
+            firstname varchar(255) NOT NULL,
+            lastname varchar(255) NOT NULL,
+            status TINYINT NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        ) ENGINE=INNODB;";
+        $db->pdo->exec($SQL);
     }
 
     public function dowm(){
-        echo "Down migration".PHP_EOL;
+        $db = app\core\Application::$app->db;
+        $SQL = "DROP TABLE users;";
+        $db->pdo->exec($SQL);
     }
 }
 ?>
---
+++ migrations/m0002_add_password_column.php
@@ -0,0 +1,13 @@
+<?php
+class m0002_add_password_column{
+    public function up(){
+        $db = app\core\Application::$app->db;
+        $db->pdo->exec("ALTER TABLE users ADD COLUMN password VARCHAR(255) NOT NULL");
+    }
+
+    public function dowm(){
+        $db = app\core\Application::$app->db;
+        $db->pdo->exec("ALTER TABLE users DROP COLUMN password");
+    }
+}
+?>
